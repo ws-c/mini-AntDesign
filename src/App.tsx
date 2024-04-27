01aaa519b0a030e260f301ea012d77c25bc60b73
ws <853800186@qq.com>
2024-04-27 17:13:45 +0800

    日历组件calendar

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import { Calendar } from './components/Calender'
+import './index.css'
 function App() {
+  const onchange = (date: Date) => {
+    console.log(date)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Calendar onChange={onchange}></Calendar>
     </div>
-  );
+  )
 }
-
-export default App;
+export default App
---
+++ src/components/Calender.tsx
@@ -0,0 +1,115 @@
+import style from './Calendar.module.css'
+import React, { useState } from 'react'
+
+interface CalendarProps {
+  value?: Date
+  onChange?: (date: Date) => void
+}
+
+export const Calendar: React.FC<CalendarProps> = (props) => {
+  const { value = new Date(), onChange } = props
+
+  const [date, setDate] = useState(value)
+  const handlePrevMonth = () => {
+    setDate(new Date(date.getFullYear(), date.getMonth() - 1, 1))
+  }
+
+  const handleNextMonth = () => {
+    setDate(new Date(date.getFullYear(), date.getMonth() + 1, 1))
+  }
+  const monthNames = [
+    '一月',
+    '二月',
+    '三月',
+    '四月',
+    '五月',
+    '六月',
+    '七月',
+    '八月',
+    '九月',
+    '十月',
+    '十一月',
+    '十二月',
+  ]
+  // 获取当月总天数
+  const daysOfMonth = (year: number, month: number) => {
+    return new Date(year, month + 1, 0).getDate()
+    // 0 代表上個月的最後一天
+  }
+  // 获取现在是星期几
+  const firstDayOfMonth = (year: number, month: number) => {
+    return new Date(year, month, 1).getDay()
+    // 0 代表星期日
+  }
+  // 获取上个月的天数
+  const daysOfLastMonth = (year: number, month: number) => {
+    return new Date(year, month, 0).getDate()
+  }
+  const renderDays = () => {
+    const days = []
+
+    const clickHandler = (i: number) => {
+      const newDate = new Date(date.getFullYear(), date.getMonth(), i)
+      setDate(newDate)
+      onChange && onChange(newDate)
+    }
+
+    const daysCount = daysOfMonth(date.getFullYear(), date.getMonth())
+
+    const firstDay = firstDayOfMonth(date.getFullYear(), date.getMonth())
+
+    const lastMonthDays =
+      daysOfLastMonth(date.getFullYear(), date.getMonth()) - firstDay + 1
+
+    for (let i = 0; i < firstDay; i++) {
+      days.push(
+        <div key={`gray-${i}`} className={style.gray}>
+          {lastMonthDays + i}
+        </div>
+      )
+    }
+
+    for (let i = 1; i <= daysCount; i++) {
+      if (i === date.getDate()) {
+        days.push(
+          <div
+            key={i}
+            className={`${style.day} ${style.selected}`}
+            onClick={() => clickHandler(i)}
+          >
+            {i}
+          </div>
+        )
+      } else {
+        days.push(
+          <div key={i} className={style.day} onClick={() => clickHandler(i)}>
+            {i}
+          </div>
+        )
+      }
+    }
+
+    return days
+  }
+  return (
+    <div className={style.calendar}>
+      <div className={style.header}>
+        <button onClick={handlePrevMonth}>&lt;</button>
+        <div>
+          {date.getFullYear()}年{monthNames[date.getMonth()]}
+        </div>
+        <button onClick={handleNextMonth}>&gt;</button>
+      </div>
+      <div className={style.days}>
+        <div className={style.day}>日</div>
+        <div className={style.day}>一</div>
+        <div className={style.day}>二</div>
+        <div className={style.day}>三</div>
+        <div className={style.day}>四</div>
+        <div className={style.day}>五</div>
+        <div className={style.day}>六</div>
+        {renderDays()}
+      </div>
+    </div>
+  )
+}
